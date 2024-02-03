f4a755fd467945e29d9149194a75e42ab28dc8a8
Rodrigo Cesar Panini <rodrigo_panini@hotmail.com>
2024-02-02 22:17:26 -0300

    Criando a classe pessoa

---
+++ ExemploAPICSharp/ExemploAPICSharp/Controllers/PessoaController.cs
@@ -0,0 +1,22 @@
+﻿using ExemploAPICSharp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExemploAPICSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PessoaController : ControllerBase
+    {
+        [HttpGet("GetPessoa")]
+        public Pessoa GetPessoa()
+        {
+            var vPessoa = new Pessoa();
+            vPessoa.Id = 1;
+            vPessoa.Nome = "José das Couves";
+            vPessoa.NumeroCelular = "(18) 98569-7845";
+
+            return vPessoa;
+        }
+    }
+}
---
+++ ExemploAPICSharp/ExemploAPICSharp/Model/Pessoa.cs
@@ -0,0 +1,10 @@
+﻿namespace ExemploAPICSharp.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string NumeroCelular { get; set; }
+        public DateOnly DataNascimento { get; set; }
+    }
+}
